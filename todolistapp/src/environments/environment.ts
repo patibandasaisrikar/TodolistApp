ab58f1ff20f1f9606269f61a455d85780c4cb469
unknown <ssrikar.p@gmail.com>
2020-11-18 23:38:51 -0500

    first commit todoapp

---
+++ todolistapp/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDvRG8mP1FFZgcyi42GRVoLjX5fx3Q-8HI",
+    authDomain: "todolistproj-1ee51.firebaseapp.com",
+    databaseURL: "https://todolistproj-1ee51.firebaseio.com",
+    projectId: "todolistproj-1ee51",
+    storageBucket: "todolistproj-1ee51.appspot.com",
+    messagingSenderId: "469449828360",
+    appId: "1:469449828360:web:6421e5e35cca1ba06a09bd"
+  }
+};
